4f3a2bf0f6775d77c30062a9bf32cba137e0554f
udu3324 <47045986+udu3324@users.noreply.github.com>
2021-03-30 08:09:35 -0500

    yes

--- src/com/codebind/saveState.java
+++ src/com/codebind/saveState.java
@@ -1,5 +1,7 @@
 package com.codebind;
 
+import javax.swing.*;
+
 public class saveState {
     //version
     //format: #major.#minor.#patch [A]describe beta, alpha, patch, release, etc.
@@ -8,4 +10,10 @@ public class saveState {
     //clickerCounter
     public int count = 0;
 
+    //clickerTimer
+    //timer stuff
+    public final Timer timer; //create timer
+    public int delay = 300; //delay of when the time starts
+    public long duration = 10000; //duration of time
+    public long startTime = -1; //start of the time (do not remove)
 }
--- src/com/ui2/clickTimerUI.java
+++ src/com/ui2/clickTimerUI.java
@@ -5,6 +5,7 @@ import com.codebind.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
 
 public class clickTimerUI {
     public JPanel rootPanel;
@@ -21,6 +22,31 @@ public class clickTimerUI {
         saveState save = new saveState();
 
         versionLabel.setText("v" + save.version);
+
+        save.timer = new Timer(20, new ActionListener() { //timer module
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (save.startTime < 0) { //if time reaches 0, stop time so it doesn't go to negative int
+                    save.startTime = System.currentTimeMillis(); //use system time
+                }
+                long now = System.currentTimeMillis(); //use system time
+                long clockTime = now - save.startTime;
+                if (clockTime >= save.duration) { //whenever clock reaches 0, run command under:
+                    clockTime = save.duration;
+                    save.timer.stop(); //stop the timer from going to the negatives
+
+                    //replace with diff label
+                    hack.setEnabled(false); //disables hack button as timer went to 0
+                    reset.setEnabled(true); //enable reset button to play again
+
+                }
+                SimpleDateFormat df = new SimpleDateFormat("mm:ss.SSS"); //format of time shown
+
+                //replace with diff label
+                time.setText(df.format(save.duration - clockTime)); //set time component to destination
+            }
+        });
+        save.timer.setInitialDelay(save.delay); //set the delay
 
         clickerTimerButton.addActionListener(new ActionListener() {
             @Override
